f19cf87301452c5fc55d3298aa909e52c7ca1482
11AlexanderB <bezhenar93@internet.ru>
2022-09-01 19:05:47 +0300

    Степень

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int DegreeOf()
+{
+    Console.WriteLine("Введите число A: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    int Deg = 1;
+
+    for(int i =1; i<=B; i++) Deg = Deg * A;
+    return Deg;
+}
+Console.WriteLine(DegreeOf());
