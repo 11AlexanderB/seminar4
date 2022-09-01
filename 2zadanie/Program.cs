ca0ffcd320b34296c708c87f6037189f013e6dc8
11AlexanderB <bezhenar93@internet.ru>
2022-09-01 19:10:01 +0300

    SumDigit

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigit()
+{
+    Console.WriteLine("Введите число: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int Sum = 0;
+    while(A>0)
+    {
+        Sum = Sum + A%10;
+        A = A/10;
+    }
+    return Sum;
+}
+Console.WriteLine(SumDigit());
