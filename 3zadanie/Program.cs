f0b9b090ce5d49beb0ed4902277d05a72a139e8c
11AlexanderB <bezhenar93@internet.ru>
2022-09-01 19:13:34 +0300

    Massiv

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [N];
+
+for(int i = 0; i <= N; i++)
+{
+    numbers[i] =  new Random(). Next(0, 100);
+    if(i != N-1) Console.Write($"{numbers[i]}, ");
+    else Console.Write($"{numbers[i]}    ");
+}
